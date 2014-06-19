a65b8b03d97c210ea91b1baf975be42769855f2e
TristanHuber <hubert4@uw.edu>
2014-06-19 14:26:28 -0700

    draw squares!

--- draw.js
+++ draw.js
@@ -1,3 +1,16 @@
 $(function(){
-  alert("loaded js");
+  $('#canvas').click(drawBox);
 });
+
+
+function drawBox(e){
+  var canvas = e.target;
+  jcanvas = $(canvas);
+  
+  x = e.pageX - jcanvas.position().left;
+  y = e.pageY - jcanvas.position().top;
+  
+  var ctx=canvas.getContext('2d');
+  ctx.fillStyle='#FF0000';
+  ctx.fillRect(x,y,20,20);
+}
