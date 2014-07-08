cbde45f5ffcf54587701033dacf15da7cfbd2d1a
TristanHuber <hubert4@uw.edu>
2014-07-08 14:16:42 -0700

    dotwise lines

--- draw.js
+++ draw.js
@@ -1,16 +1,26 @@
+var drawing = false;
+
 $(function(){
-  $('#canvas').click(drawBox);
+  $('#canvas').click(onebox);
+  $('#canvas').mousedown(function (e) { drawing = true; });
+  $('#canvas').mousemove(onebox);
+  $('#canvas').mouseup(function(e) { drawing = false; });
 });
 
-
-function drawBox(e){
-  var canvas = e.target;
-  jcanvas = $(canvas);
-  
-  x = e.pageX - jcanvas.position().left;
-  y = e.pageY - jcanvas.position().top;
-  
+function drawBox(x,y){
   var ctx=canvas.getContext('2d');
   ctx.fillStyle='#FF0000';
-  ctx.fillRect(x,y,20,20);
+  ctx.fillRect(x,y,5,5);
+}
+
+function onebox(e){
+  if(drawing){
+
+    var canvas = e.target;
+    jcanvas = $(canvas);
+  
+    x = e.pageX - jcanvas.offset().left;
+    y = e.pageY - jcanvas.offset().top;
+    drawBox(x, y);  
+  }
 }
